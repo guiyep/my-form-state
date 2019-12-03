cde9d88f8352c71483234cb175d8d588e99a3526
Guille <guiyep@gmail.com>
2019-12-04 08:04:54 +1100

    adding jsdoc

--- src/redux/forms/actions.js
+++ src/redux/forms/actions.js
@@ -1,5 +1,37 @@
 import { VALIDATED_FORM, UPDATE_FORM, SUBMIT_FORM, CLEAR_FORM, INITIALIZE_FORM, RESET_FORM } from './types';
 
+/**
+ * A Redux Action.
+ *
+ * @module my-form-state/redux
+ * @typedef {object} Action.
+ * @property {type} type - The action type.
+ * @property {object} payload - The changes to be sent to the reducer.
+ * @property {object} options.
+ * @property {formId} options.formId - The unique form Id.
+ */
+
+/**
+ * Will return an action that will validate the form state based on your validation function and update the form state and props. This is normally
+ * handled by the operations.
+ *
+ * @module my-form-state/redux
+ * @param {object} Arguments - Arguments as object.
+ * @param {object} [Arguments.errors] - key value pair { [field] : error } with the changes.
+ * @param {string} Arguments.formId - the unique form id indicator.
+ * @returns {Action} an action to be executed using dispatch.
+ * @throws Arguments.formId is falsy
+ *
+ * @example
+ *
+ *     dispatch(validateForm({
+ *        error: {
+ *          color: 'color need to be defined'
+ *        },
+ *        formId: 'unique-form-id'
+ *     }))
+ */
+
 export const validateForm = ({ errors, formId }) => ({
   type: VALIDATED_FORM,
   payload: typeof errors === 'object' ? errors : {},
@@ -7,6 +39,27 @@ export const validateForm = ({ errors, formId }) => ({
     formId,
   },
 });
+
+/**
+ * Will return an action that will update the my-form form state state with the changes once executed using dispatch.
+ * Will also update all the isSubmitted, isValid, etc. props of the form and the fields based on the change.
+ *
+ * @module my-form-state/redux
+ * @param {object} Arguments - Arguments as object.
+ * @param {object} [Arguments.data] - key value pair { [field] : value } with the changes.
+ * @param {string} Arguments.formId - the unique form id indicator.
+ * @returns {Action} an action to be executed using dispatch.
+ * @throws Arguments.formId is falsy
+ *
+ * @example
+ *
+ *     dispatch(updateForm({
+ *        data: {
+ *          color: 'blue'
+ *        },
+ *        formId: 'unique-form-id'
+ *     }))
+ */
 
 export const updateForm = ({ data, formId }) => ({
   type: UPDATE_FORM,
@@ -16,12 +69,49 @@ export const updateForm = ({ data, formId }) => ({
   },
 });
 
+/**
+ * Will submit the changes to the form and lock them.
+ * Will also update all the isSubmitted, isValid, etc. props of the form and the fields based on the change.
+ *
+ * @module my-form-state/redux
+ * @param {object} Arguments - Arguments as object.
+ * @param {string} Arguments.formId - the unique form id indicator.
+ * @returns {Action} an action to be executed using dispatch.
+ * @throws Arguments.formId is falsy
+ *
+ * @example
+ *
+ *     dispatch(submitForm({
+ *        formId: 'unique-form-id'
+ *     }))
+ */
+
 export const submitForm = ({ formId }) => ({
   type: SUBMIT_FORM,
   options: {
     formId,
   },
 });
+
+/**
+ * This initialize the form internal state and add it to the forms store. Normally this will be executed after the
+ * component has been mounted. It is a good time for sending your initial form values.
+ *
+ * @module my-form-state/redux
+ * @param {object} Arguments - Arguments as object.
+ * @param {object} [Arguments.initialState] - your form initial state as { [props] : value }.
+ * @param {string} Arguments.formId - the unique form id indicator.
+ * @returns {Action} an action to be executed using dispatch.
+ * @throws Arguments.formId is falsy
+ * @example
+ *
+ *     dispatch(initializeForm({
+ *        initialState: {
+ *          color: 'red'
+ *        },
+ *        formId: 'unique-form-id'
+ *     }))
+ */
 
 export const initializeForm = ({ initialState, formId }) => ({
   type: INITIALIZE_FORM,
@@ -31,12 +121,46 @@ export const initializeForm = ({ initialState, formId }) => ({
   },
 });
 
+/**
+ * This remove all references to the form in your store. Normally will be executed when your component was unmounted.
+ *
+ * @module my-form-state/redux
+ * @param {object} Arguments - Arguments as object.
+ * @param {string} Arguments.formId - the unique form id indicator.
+ * @returns {Action} an action to be executed using dispatch.
+ * @throws Arguments.formId is falsy
+ *
+ * @example
+ *
+ *     dispatch(clearForm({
+ *        formId: 'unique-form-id'
+ *     }))
+ */
+
 export const clearForm = ({ formId }) => ({
   type: CLEAR_FORM,
   options: {
     formId,
   },
 });
+
+/**
+ * This will reset the form and all its internal props to it original state. Normally used if we want to re-use the current
+ * form after was submitted. You can re-initialize the form with another initial state if you want.
+ *
+ * @module my-form-state/redux
+ * @param {object} Arguments - Arguments as object.
+ * @param {object} [Arguments.initialState] - your form initial state as { [props] : value }.
+ * @param {string} Arguments.formId - the unique form id indicator.
+ * @returns {Action} an action to be executed using dispatch.
+ * @throws Arguments.formId is falsy
+ *
+ * @example
+ *
+ *     dispatch(resetForm({
+ *        formId: 'unique-form-id'
+ *     }))
+ */
 
 export const resetForm = ({ formId, initialState }) => ({
   type: RESET_FORM,
--- src/redux/forms/operations.js
+++ src/redux/forms/operations.js
@@ -13,7 +13,27 @@ import {
   resetForm as resetFormAction,
 } from './actions';
 
+/**
+ * Will validate the form state based on your validation function and update the form state and props.
+ *
+ * @module my-form-state/redux
+ * @param {object} Arguments - Arguments as object.
+ * @param {string} Arguments.formId - the unique form id indicator.
+ * @return {function} a function to be executed passing dispatch and getState that returns a thunk.
+ * @throws Arguments.formId is falsy
+ *
+ * @example
+ *
+ *     dispatch(validateForm({
+ *        formId: 'unique-form-id'
+ *     }))
+ */
+
 export const validateForm = ({ formId }) => (dispatch, getState) => {
+  if (!formId || !dispatch || !getState) {
+    throw new Error('you need to pass the formId, dispatch, getState');
+  }
+
   const formData = getFormFromRegistry(formId);
   const validatorFunction = formData.formValidator;
 
@@ -35,7 +55,31 @@ export const validateForm = ({ formId }) => (dispatch, getState) => {
   return true;
 };
 
+/**
+ * Will update the field state inside the form, after that will validate the form and update all the form state and field props.
+ *
+ * @module my-form-state/redux
+ * @param {object} Arguments - Arguments as object.
+ * @param {string} Arguments.formId - the unique form id indicator.
+ * @param {string} Arguments.field - the field name inside the form.
+ * @param {any} Arguments.value - any value.
+ * @returns {function} a function to be executed passing dispatch and getState that returns a thunk.
+ * @throws Arguments.formId is falsy
+ * @throws Arguments.field is falsy
+ *
+ * @example
+ *
+ *     dispatch(updateField({
+ *        field: 'color',
+ *        value: 'red',
+ *        formId: 'unique-form-id'
+ *     }))
+ */
 export const updateField = ({ formId, field, value }) => (dispatch) => {
+  if (!formId || !field || !dispatch) {
+    throw new Error('you need to pass the formId, field and dispatch');
+  }
+
   dispatch(
     updateForm({
       formId,
@@ -55,31 +99,138 @@ const validateFormDebounced = debounce(
   { leading: false, trailing: true },
 );
 
+/**
+ * Will update the form data, validate the form and update all the form state and field props.
+ *
+ * @module my-form-state/redux
+ * @param {object} Arguments - Arguments as object.
+ * @param {object} Arguments.data -  a ke value pair with the form data.
+ * @param {string} Arguments.formId - the unique form id indicator.
+ * @returns {function} a function to be executed passing dispatch and getState that returns a thunk.
+ * @throws Arguments.formId is falsy
+ * @throws Arguments.data is falsy
+ *
+ * @example
+ *
+ *     dispatch(updateForm({
+ *        data: {
+ *          color: 'red'
+ *        },
+ *        formId: 'unique-form-id'
+ *     }))
+ */
+
 export const updateForm = ({ formId, data }) => (dispatch) => {
+  if (!formId || !data || !dispatch) {
+    throw new Error('you need to pass the formId, data and dispatch');
+  }
+
   dispatch(updateFormAction({ data, formId }));
 
   validateFormDebounced(formId, dispatch);
 };
 
-export const submitForm = ({ formId }) => async (dispatch) => {
+/**
+ * Will submit and lock the form.
+ * You can await for this operation and will resolve the promise once the validation is completed after the form is submitted.
+ *
+ * @module my-form-state/redux
+ * @param {object} Arguments - Arguments as object.
+ * @param {string} Arguments.formId - the unique form id indicator.
+ * @returns {function} a function to be executed passing dispatch and getState that returns a thunk.
+ * @throws Arguments.formId is falsy
+ *
+ * @example
+ *
+ *     dispatch(submitForm({
+ *        formId: 'unique-form-id'
+ *     }))
+ */
+
+export const submitForm = ({ formId }) => (dispatch) => {
+  if (!formId || !dispatch) {
+    throw new Error('you need to pass the formId and dispatch');
+  }
+
   dispatch(submitFormAction({ formId }));
 
-  const result = await dispatch(validateForm({ formId }));
-  return result;
+  return dispatch(validateForm({ formId }));
 };
 
-export const initializeForm = ({ formId, initialState }) => async (dispatch) => {
+/**
+ * Will initialize the form. Normally use after the component that uses this operation is mounted.
+ * You can await for this operation and will resolve the promise once the validation is completed after the form is initialized.
+ *
+ * @module my-form-state/redux
+ * @param {object} Arguments - Arguments as object.
+ * @param {string} Arguments.formId - the unique form id indicator.
+ * @param {object} [Arguments.initialState] - the form initial state
+ * @returns {function} a function to be executed passing dispatch and getState that returns a thunk.
+ * @throws Arguments.formId is falsy
+ *
+ * @example
+ *
+ *     dispatch(initializeForm({
+ *        formId: 'unique-form-id'
+ *     }))
+ *
+ */
+
+export const initializeForm = ({ formId, initialState = {} }) => (dispatch) => {
+  if (!formId || !dispatch) {
+    throw new Error('you need to pass the formId and dispatch');
+  }
+
   dispatch(initializeFormAction({ initialState, formId }));
 
   // it is not always the case we need to validate the schema on initialization
-  dispatch(validateForm({ formId }));
+  return dispatch(validateForm({ formId }));
 };
+
+/**
+ * Will clear the form state from the store. Normally use after the component that uses this operation is unmounted.
+ *
+ * @module my-form-state/redux
+ * @param {object} Arguments - Arguments as object.
+ * @param {string} Arguments.formId - the unique form id indicator.
+ * @returns {function} a function to be executed passing dispatch and getState that returns a thunk.
+ * @throws Arguments.formId is falsy
+ *
+ * @example
+ *
+ *     dispatch(clearForm({
+ *        formId: 'unique-form-id'
+ *     }))
+ */
 
 export const clearForm = ({ formId }) => (dispatch) => {
-  dispatch(clearFormAction({ formId }));
+  if (!formId || !dispatch) {
+    throw new Error('you need to pass the formId and dispatch');
+  }
+  return dispatch(clearFormAction({ formId }));
 };
 
+/**
+ * Will reset the form state to the initial state. You can also change the initialState using the operation.
+ *
+ * @module my-form-state/redux
+ * @param {object} Arguments - Arguments as object.
+ * @param {string} Arguments.formId - the unique form id indicator.
+ * @param {object} [Arguments.initialState] - the form initial state
+ * @returns {function} a function to be executed passing dispatch and getState that returns a thunk.
+ * @throws Arguments.formId is falsy
+ *
+ * @example
+ *
+ *     dispatch(clearForm({
+ *        formId: 'unique-form-id'
+ *     }))
+ */
+
 export const resetForm = ({ formId, initialState }) => (dispatch) => {
+  if (!formId || !dispatch) {
+    throw new Error('you need to pass the formId and dispatch');
+  }
   const formData = getFormFromRegistry(formId);
   dispatch(clearFormAction({ formId }));
   dispatch(resetFormAction({ formId, initialState: initialState || formData.initialState }));

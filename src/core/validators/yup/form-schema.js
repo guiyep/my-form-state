e34a48045b5ab0f8acef8affd622af9802c43123
Guille <guiyep@gmail.com>
2020-01-22 08:13:13 +1100

    adding jsdoc

--- src/core/validators/yup/form-schema.js
+++ src/core/validators/yup/form-schema.js
@@ -3,6 +3,29 @@ import { formValidator as formValidatorSync } from './form-validator/sync';
 import { formValidator as formValidatorAsync } from './form-validator/async';
 import { jsonSchema } from './json-schema';
 
+/**
+ * Builds a function that `my-form-library` will use to validate the YUP schema.
+ *
+ * @kind function
+ * @name formSchema
+ * @param {Object} schema - a YUP schema object.
+ * @param {Object} options - options as object.
+ * @param {string} [options.async=false] - if the validation need to happen sync or async.
+ * @returns {Function} a function that will be used inside for validating the form.
+ * @throws Arguments.schema is falsy
+ *
+ * @example 1
+ *  const [formState, { updateField, submitForm, resetForm }] = useMyFormState({
+ *    initialState,
+ *    formSchema: formSchema(schema, { async: true }),
+ *  });
+ *
+ * @example 2
+ *  const [formState, { updateField, submitForm, resetForm }] = useMyFormState({
+ *    initialState,
+ *    formSchema: formSchema(schema),
+ *  });
+ */
 export const formSchema = (schema, { async = false } = {}) => {
   ParamValidator.isObject(schema, 'schema');
 

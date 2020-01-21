a31a4e664db91b91f49cc109e3f66d0175460004
Guille <guiyep@gmail.com>
2020-01-22 08:15:17 +1100

    fixing jsdoc

--- src/core/validators/yup/form-schema.js
+++ src/core/validators/yup/form-schema.js
@@ -10,9 +10,9 @@ import { jsonSchema } from './json-schema';
  * @name formSchema
  * @param {Object} schema - a YUP schema object.
  * @param {Object} [options] - options as object.
- * @param {string} [options.async=false] - if the validation need to happen sync or async.
+ * @param {boolean} [options.async=false] - if the validation need to happen sync or async.
  * @returns {Function} a function that will be used inside for validating the form.
- * @throws Arguments.schema is falsy
+ * @throws schema is falsy
  *
  * @example 1
  *  const [formState, { updateField, submitForm, resetForm }] = useMyFormState({
